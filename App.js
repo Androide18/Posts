6ad85795abe208d9dc863e114e7d480a49038c04
Androide18 <luciana.ayala1@gmail.com>
2022-04-04 16:49:06 -0300

    screens

--- App.js
+++ App.js
@@ -1,48 +1,24 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import UsersScreen from "./screens/Users";
+import PostsScreen from "./screens/Posts";
+import DetailScreen from "./screens/Detail";
 
-function PostsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola desde React Native :) </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-function DetailScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola desde React Native :) </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const AppNavigator = createStackNavigator({
-  Users: {
-    screen: UsersScreen,
+const AppNavigator = createStackNavigator(
+  {
+    Users: {
+      screen: UsersScreen,
+    },
+    Posts: {
+      screen: PostsScreen,
+    },
+    Detail: {
+      screen: DetailScreen,
+    },
   },
-  Posts: {
-    screen: PostsScreen,
-  },
-  Detail: {
-    screen: DetailScreen,
-  },
-}, {
-  initialRouteName: 'Users',
-});
+  {
+    initialRouteName: "Users",
+  }
+);
 
 export default createAppContainer(AppNavigator);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
