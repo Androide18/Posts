c0f74b959c39e0c3570e618426fa58b80d4dea99
Androide18 <luciana.ayala1@gmail.com>
2022-04-04 16:37:12 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import UsersScreen from "./screens/Users";
 
-export default function App() {
+function PostsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola desde React Native :) </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+function DetailScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola desde React Native :) </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const AppNavigator = createStackNavigator({
+  Users: {
+    screen: UsersScreen,
+  },
+  Posts: {
+    screen: PostsScreen,
+  },
+  Detail: {
+    screen: DetailScreen,
+  },
+}, {
+  initialRouteName: 'Users',
+});
+
+export default createAppContainer(AppNavigator);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
